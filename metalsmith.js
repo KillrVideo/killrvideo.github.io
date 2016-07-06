3b40ff0b07adadaff40b72d0a7f5107388643cc6
Luke Tillman <luke.tillman@gmail.com>
2016-07-05 18:09:25 -0600

    Add handlebars templating, default template

--- metalsmith.js
+++ metalsmith.js
@@ -1,11 +1,20 @@
 const path = require('path');
 const Metalsmith = require('metalsmith');
 const watch = require('metalsmith-watch');
+const layouts = require('metalsmith-layouts');
+const paths = require('metalsmith-paths');
 
 // Build paths
 const Paths = {
-  SRC: path.resolve(__dirname, 'src/site'),
+  SITE: path.resolve(__dirname, 'src/site'),
+  LAYOUTS: path.resolve(__dirname, 'src/layouts'),
   OUT: path.resolve(__dirname, 'out')
+};
+
+// Global metadata available to all files
+const GLOBAL_METADATA = {
+  "github_org_url": "https://github.com/KillrVideo",
+  "github_site_url": "https://github.com/KillrVideo/killrvideo.github.io"
 };
 
 /**
@@ -14,8 +23,17 @@ const Paths = {
 
 let ms = Metalsmith(__dirname)
   .clean(false)
-  .source(Paths.SRC)
-  .destination(Paths.OUT);
+  .source(Paths.SITE)
+  .destination(Paths.OUT)
+  .metadata(GLOBAL_METADATA)
+  // Add the original path info to metadata before any processing is done
+  .use(paths({ property: "original_path" }))
+  // Use handlebars layouts on all site pages
+  .use(layouts({
+    engine: 'handlebars',
+    default: 'default.hbs',
+    directory: Paths.LAYOUTS
+  }));
 
 // Should we watch?
 if (process.argv.length > 2 && process.argv[2] === '--watch') {
@@ -25,5 +43,5 @@ if (process.argv.length > 2 && process.argv[2] === '--watch') {
 // Execute the build
 ms.build(function(err) {
   if (err) throw err;
-  console.log('Build complete');
+  console.log('Site build complete');
 });
