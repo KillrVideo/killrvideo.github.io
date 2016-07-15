2228a370eb41f9775d85b4a4fdd8574456237859
Luke Tillman <luke.tillman@gmail.com>
2016-07-15 17:10:31 -0600

    Add SASS compile to build

--- Brocfile.js
+++ Brocfile.js
@@ -1,11 +1,20 @@
 const Funnel = require('broccoli-funnel');
+const Sass = require('broccoli-sass');
+const MergeTrees = require('broccoli-merge-trees');
 
 const Paths = {
-  IMAGES: 'src/images'
+  NODE_MODULES: 'node_modules',
+  IMAGES: 'src/images',
+  SASS: 'src/sass'
 };
 
+// Copy images to output as-is
 const imageFiles = new Funnel(Paths.IMAGES, {
   destDir: 'images'
 });
 
-module.exports = imageFiles;
+// Compile SASS to CSS
+const cssFiles = new Sass([ Paths.SASS, Paths.NODE_MODULES ], 'bundle.scss', 'css/bundle.css');
+
+// Merge output
+module.exports = new MergeTrees([ imageFiles, cssFiles ]);
