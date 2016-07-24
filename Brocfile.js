a29487975482402fba7eba64278490a0c91992e1
Luke Tillman <luke.tillman@gmail.com>
2016-07-24 12:20:54 -0600

    Speed up build by not using node_modules as input

--- Brocfile.js
+++ Brocfile.js
@@ -1,14 +1,20 @@
+const resolve = require('resolve');
+const path = require('path');
+
 const Funnel = require('broccoli-funnel');
 const Sass = require('broccoli-sass');
 const Markdown = require('broccoli-marked');
 const MergeTrees = require('broccoli-merge-trees');
 
 const Paths = {
-  NODE_MODULES: 'node_modules',
   IMAGES: 'src/images',
+  BULMA: path.dirname(resolve.sync('bulma')),
+  FONT_AWESOME: path.dirname(resolve.sync('font-awesome/css/font-awesome.css')),
   SASS: 'src/sass',
   SITE: 'src/site'
 };
+
+console.log(Paths.FONT_AWESOME);
 
 // Copy images to output as-is
 const imageFiles = new Funnel(Paths.IMAGES, {
@@ -16,11 +22,11 @@ const imageFiles = new Funnel(Paths.IMAGES, {
 });
 
 // Compile SASS to CSS
-const cssFiles = new Sass([ Paths.SASS, Paths.NODE_MODULES ], 'bundle.scss', 'css/bundle.css');
+const cssFiles = new Sass([ Paths.SASS, Paths.BULMA, Paths.FONT_AWESOME ], 'bundle.scss', 'css/bundle.css');
 
 // Convert markdown to HTML
-let markdownFiles = new Funnel(Paths.SITE, { include: [ '**/*.md' ] });
-markdownFiles = new Markdown(markdownFiles, { gfm: true });
+const markdownFiles = new Funnel(Paths.SITE, { include: [ '**/*.md' ] });
+const markdownHtml = new Markdown(markdownFiles, { gfm: true });
 
 // Merge output
-module.exports = new MergeTrees([ imageFiles, cssFiles, markdownFiles ]);
+module.exports = new MergeTrees([ imageFiles, cssFiles, markdownHtml ]);
