0eb23a50c04fc8c9533c58f4810893eb40157d19
Luke Tillman <luke.tillman@gmail.com>
2016-07-24 11:17:21 -0600

    Convert markdown to HTML

--- Brocfile.js
+++ Brocfile.js
@@ -1,11 +1,13 @@
 const Funnel = require('broccoli-funnel');
 const Sass = require('broccoli-sass');
+const Markdown = require('broccoli-marked');
 const MergeTrees = require('broccoli-merge-trees');
 
 const Paths = {
   NODE_MODULES: 'node_modules',
   IMAGES: 'src/images',
-  SASS: 'src/sass'
+  SASS: 'src/sass',
+  SITE: 'src/site'
 };
 
 // Copy images to output as-is
@@ -16,5 +18,9 @@ const imageFiles = new Funnel(Paths.IMAGES, {
 // Compile SASS to CSS
 const cssFiles = new Sass([ Paths.SASS, Paths.NODE_MODULES ], 'bundle.scss', 'css/bundle.css');
 
+// Convert markdown to HTML
+let markdownFiles = new Funnel(Paths.SITE, { include: [ '**/*.md' ] });
+markdownFiles = new Markdown(markdownFiles, { gfm: true });
+
 // Merge output
-module.exports = new MergeTrees([ imageFiles, cssFiles ]);
+module.exports = new MergeTrees([ imageFiles, cssFiles, markdownFiles ]);
