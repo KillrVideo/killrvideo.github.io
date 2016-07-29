81acc71306f4591f265cb04ce391807cb32a5162
Travis CI <travisci@killrvideo.com>
2016-07-29 14:34:16 +0000

    Deploy to GitHub Pages: 770ece5b2b38d354e77857dc7e09c08ff729e625

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -43,15 +43,36 @@ var KillrVideo =
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ function(module, exports) {
+/***/ function(module, exports, __webpack_require__) {
 
-	"use strict";
+	'use strict';
 	
 	Object.defineProperty(exports, "__esModule", {
 	  value: true
 	});
 	exports.someFunction = someFunction;
+	
+	// Include Google Analytics when in production
+	if (true) {
+	  var ga = __webpack_require__(1);
+	}
+	
 	function someFunction() {};
+
+/***/ },
+/* 1 */
+/***/ function(module, exports) {
+
+	'use strict';
+	
+	(function (i, s, o, g, r, a, m) {
+	  i['GoogleAnalyticsObject'] = r;i[r] = i[r] || function () {
+	    (i[r].q = i[r].q || []).push(arguments);
+	  }, i[r].l = 1 * new Date();a = s.createElement(o), m = s.getElementsByTagName(o)[0];a.async = 1;a.src = g;m.parentNode.insertBefore(a, m);
+	})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+	
+	ga('create', 'UA-54473346-2', 'auto');
+	ga('send', 'pageview');
 
 /***/ }
 /******/ ]);
