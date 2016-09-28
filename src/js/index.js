82614ac6b01353404994f45a9df25a5d74904aba
Luke Tillman <luke.tillman@gmail.com>
2016-09-28 14:35:02 -0600

    Add mobile menu listeners (Fixes #12)
    - Also update some of the styling on the mobile menu to match the rest of the site

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,16 @@
+import $ from 'cash-dom';
 import { sticky } from './sticky';
 
 // Include Google Analytics when in production
 if (process.env.NODE_ENV === 'production') {
   const ga = require('./google-analytics');
 }
+
+// Add listeners for click on mobile menu
+let $mainNavMenu = $('header > nav.nav div.nav-menu');
+$('header > nav.nav span.nav-toggle').on('click', function toggleMenuActive(e) {
+  $mainNavMenu.toggleClass('is-active');
+});
 
 export {
   sticky
