28e6f98a040129c3ab116a859a4320c323e6d6b0
Travis CI <travisci@killrvideo.com>
2016-09-28 20:37:55 +0000

    Deploy to GitHub Pages: 82614ac6b01353404994f45a9df25a5d74904aba

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -52,118 +52,29 @@ var KillrVideo =
 	});
 	exports.sticky = undefined;
 	
-	var _sticky = __webpack_require__(1);
+	var _cashDom = __webpack_require__(1);
+	
+	var _cashDom2 = _interopRequireDefault(_cashDom);
+	
+	var _sticky = __webpack_require__(2);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 	
 	// Include Google Analytics when in production
 	if (true) {
 	  var ga = __webpack_require__(4);
 	}
 	
+	// Add listeners for click on mobile menu
+	var $mainNavMenu = (0, _cashDom2.default)('header > nav.nav div.nav-menu');
+	(0, _cashDom2.default)('header > nav.nav span.nav-toggle').on('click', function toggleMenuActive(e) {
+	  $mainNavMenu.toggleClass('is-active');
+	});
+	
 	exports.sticky = _sticky.sticky;
 
 /***/ },
 /* 1 */
-/***/ function(module, exports, __webpack_require__) {
-
-	'use strict';
-	
-	Object.defineProperty(exports, "__esModule", {
-	  value: true
-	});
-	exports.sticky = sticky;
-	
-	var _cashDom = __webpack_require__(2);
-	
-	var _cashDom2 = _interopRequireDefault(_cashDom);
-	
-	var _lodash = __webpack_require__(3);
-	
-	var _lodash2 = _interopRequireDefault(_lodash);
-	
-	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-	
-	/**
-	 * Helper function to determine whether or not the current page is being viewed in a mobile 
-	 * width (consistent with the breakpoints in our SASS).
-	 */
-	function getMobile() {
-	  var viewportWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-	  return viewportWidth <= 768;
-	}
-	
-	/**
-	 * Helper function to get the current scroll position of the page.
-	 */
-	function getPosition() {
-	  return window.pageYOffset || document.documentElement.scrollTop;
-	}
-	
-	/**
-	 * Helper function that determines whether given a scroll threshold, we should be putting
-	 * sticky elements into fixed positioning.
-	 */
-	function shouldBeFixedPosition(thresholdPx) {
-	  var isMobile = getMobile();
-	  if (isMobile) return false;
-	
-	  var position = getPosition();
-	  if (position < thresholdPx) return false;
-	
-	  return true;
-	}
-	
-	/**
-	 * Simulate CSS sticky positioning on an element by reacting to window scroll and resize events,
-	 * then adding a class to the specified element(s).
-	 */
-	function sticky(selector, thresholdPx, topPx) {
-	  var $el = (0, _cashDom2.default)(selector);
-	
-	  // Insert a hidden div after the element we're going to make sticky
-	  var $newDiv = (0, _cashDom2.default)(document.createElement('div')).css('height', '0px').insertAfter($el);
-	
-	  // Variable that contains state about whether or not we're in fixed position
-	  var isFixed = false;
-	
-	  // Function that will update the width of the element to be the same as the hidden div
-	  function updateWidth() {
-	    if (isFixed === false) return;
-	    $el.css('width', $newDiv.width() + 'px');
-	  }
-	
-	  // Update width function, only throttled
-	  var updateWidthOnResize = (0, _lodash2.default)(updateWidth, 100);
-	
-	  // The function we'll run as the UI is scrolled/resized to update our state
-	  function updateFixed() {
-	    // Check if we should be fixed position
-	    var shouldBeFixed = shouldBeFixedPosition(thresholdPx);
-	
-	    // If already in the correct state, just bail
-	    if (shouldBeFixed === isFixed) return;
-	
-	    // Update our state
-	    isFixed = shouldBeFixed;
-	
-	    // Update the UI
-	    if (isFixed === true) {
-	      // Set to fixed position, update the element width, and update width on resizes
-	      $el.css({ position: 'fixed', top: topPx + 'px' });
-	      updateWidth();
-	      (0, _cashDom2.default)(window).on('resize', updateWidthOnResize);
-	    } else {
-	      // Remove all style attributes and listeners
-	      $el.removeAttr('style');
-	      (0, _cashDom2.default)(window).off('resize', updateWidthOnResize);
-	    }
-	  }
-	
-	  // Update state on resize or scroll
-	  (0, _cashDom2.default)(window).on('resize', (0, _lodash2.default)(updateFixed, 100)).on('scroll', (0, _lodash2.default)(updateFixed, 100));
-	};
-
-/***/ },
-/* 2 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_RESULT__;"use strict";
@@ -1067,6 +978,107 @@ var KillrVideo =
 	
 	  return cash;
 	});
+
+/***/ },
+/* 2 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	exports.sticky = sticky;
+	
+	var _cashDom = __webpack_require__(1);
+	
+	var _cashDom2 = _interopRequireDefault(_cashDom);
+	
+	var _lodash = __webpack_require__(3);
+	
+	var _lodash2 = _interopRequireDefault(_lodash);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	/**
+	 * Helper function to determine whether or not the current page is being viewed in a mobile 
+	 * width (consistent with the breakpoints in our SASS).
+	 */
+	function getMobile() {
+	  var viewportWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+	  return viewportWidth <= 768;
+	}
+	
+	/**
+	 * Helper function to get the current scroll position of the page.
+	 */
+	function getPosition() {
+	  return window.pageYOffset || document.documentElement.scrollTop;
+	}
+	
+	/**
+	 * Helper function that determines whether given a scroll threshold, we should be putting
+	 * sticky elements into fixed positioning.
+	 */
+	function shouldBeFixedPosition(thresholdPx) {
+	  var isMobile = getMobile();
+	  if (isMobile) return false;
+	
+	  var position = getPosition();
+	  if (position < thresholdPx) return false;
+	
+	  return true;
+	}
+	
+	/**
+	 * Simulate CSS sticky positioning on an element by reacting to window scroll and resize events,
+	 * then adding a class to the specified element(s).
+	 */
+	function sticky(selector, thresholdPx, topPx) {
+	  var $el = (0, _cashDom2.default)(selector);
+	
+	  // Insert a hidden div after the element we're going to make sticky
+	  var $newDiv = (0, _cashDom2.default)(document.createElement('div')).css('height', '0px').insertAfter($el);
+	
+	  // Variable that contains state about whether or not we're in fixed position
+	  var isFixed = false;
+	
+	  // Function that will update the width of the element to be the same as the hidden div
+	  function updateWidth() {
+	    if (isFixed === false) return;
+	    $el.css('width', $newDiv.width() + 'px');
+	  }
+	
+	  // Update width function, only throttled
+	  var updateWidthOnResize = (0, _lodash2.default)(updateWidth, 100);
+	
+	  // The function we'll run as the UI is scrolled/resized to update our state
+	  function updateFixed() {
+	    // Check if we should be fixed position
+	    var shouldBeFixed = shouldBeFixedPosition(thresholdPx);
+	
+	    // If already in the correct state, just bail
+	    if (shouldBeFixed === isFixed) return;
+	
+	    // Update our state
+	    isFixed = shouldBeFixed;
+	
+	    // Update the UI
+	    if (isFixed === true) {
+	      // Set to fixed position, update the element width, and update width on resizes
+	      $el.css({ position: 'fixed', top: topPx + 'px' });
+	      updateWidth();
+	      (0, _cashDom2.default)(window).on('resize', updateWidthOnResize);
+	    } else {
+	      // Remove all style attributes and listeners
+	      $el.removeAttr('style');
+	      (0, _cashDom2.default)(window).off('resize', updateWidthOnResize);
+	    }
+	  }
+	
+	  // Update state on resize or scroll
+	  (0, _cashDom2.default)(window).on('resize', (0, _lodash2.default)(updateFixed, 100)).on('scroll', (0, _lodash2.default)(updateFixed, 100));
+	};
 
 /***/ },
 /* 3 */
